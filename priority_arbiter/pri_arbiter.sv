f453ea1c38f10b24a6ae18b21f29012bb7b3b7a3
kuchisravani <109129837+kuchisravani@users.noreply.github.com>
2022-07-18 09:41:07 +0530

    Update pri_arbiter.sv

--- priority_arbiter/pri_arbiter.sv
+++ priority_arbiter/pri_arbiter.sv
@@ -1,1 +1,16 @@
+//Parameterized priority arbiter with LSB as highest priority.
 
+module pri_arbiter #(parameter WID=4)
+ (
+   input logic [WID-1:0] req_i,
+   output logic [WID-1:0] gnt_o
+);
+  
+  assign gnt_o[0]=req_i[0];
+  
+  genvar i;
+  for(i=1;i<WID;i=i+1) begin
+    assign gnt_o[i]=req_i[i] & ~(|gnt_o[i-1:0]);
+  end
+  
+endmodule
