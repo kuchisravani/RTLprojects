7d2e1c8882c755dadcb0a37d654d48bb22833274
kuchisravani <109129837+kuchisravani@users.noreply.github.com>
2022-07-19 14:41:09 +0530

    Update syn_fifo.sv

--- FIFO/syn_fifo.sv
+++ FIFO/syn_fifo.sv
@@ -1,33 +1,35 @@
 //synchronous fifo
 
-module syn_fifo #(parameter DEP=4, parameter WID=16)
+module syn_fifo #(parameter DEP=4, parameter DWID=16)
   (
-  input wire clk,
-  input wire rst,
-  input wire wr_i,
-  input wire rd_i,
-  input wire[WID-1:0] wdata,
-  output wire[WID-1:0] rdata,
-  output wire overflow_o,
-  output wire empty_o
+  input  wire           clk,
+  input  wire           rst,
+  input  wire           wr_i,
+  input  wire           rd_i,
+  input  wire[DWID-1:0] wdata,
+  output wire[DWID-1:0] rdata,
+  output wire           overflow_o,
+  output wire           empty_o
 );
   
+  localparam PTR_WID=$clog2(DEP);
   typedef enum logic [1:0] {
     WR=2'b01, RD=2'b10, BOTH=2'b11
   } fifo_st;
   
-  logic         rd_q;
-  logic         wr_q;
-  logic [DEP:0] wrptr,wrptr_q;
-  logic [DEP:0] rdptr,rdptr_q;
+  logic             rd_q;
+  logic             wr_q;
+  logic [DWID-1:0]  rdata_q;
+  logic [PTR_WID:0] wrptr,wrptr_q;
+  logic [PTR_WID:0]rdptr,rdptr_q;
   logic [DEP-1:0][WID-1:0] fifo_mem;
   
   
-  assign empty_o=(wrptr==rdptr);
+  assign empty_o   =(wrptr==rdptr);
   assign overflow_o=(wrptr[DEP]!=rdptr[DEP]) && (wrptr[DEP-1:0]==rdptr[DEP-1:0]);
-  assign rd_q=rd_i && ~empty_o;
-  assign wr_q=wr_i && ~overflow_o;
-  assign rdata=rd_q?fifo_mem[rdptr]:0;
+  assign rd_q      =rd_i && ~empty_o;
+  assign wr_q      =wr_i && ~overflow_o;
+  assign rdata     =rdata_q;
     
   
   always_ff @(posedge clk or negedge rst) begin
@@ -43,10 +45,14 @@ module syn_fifo #(parameter DEP=4, parameter WID=16)
  always_comb begin
    wrptr_q=wrptr;
    rdptr_q=rdptr;
+   rdata_q=fifo_mem[rdptr[PTR_WID-1:0]];
    case({rd_q, wr_q}) 
-     WR  : wrptr_q++;
-     RD  : rdptr_q++;
-     BOTH: begin wrptr_q++; rdptr_q++; end
+     WR  : wrptr_q=wrptr + {PTR_WID{1'b1}};
+     RD  : rdptr_q=rdptr + {PTR_WID{1'b1}};
+     BOTH: begin 
+           wrptr_q=wrptr + {PTR_WID{1'b1}}; 
+           rdptr_q=rdptr + {PTR_WID{1'b1}};
+           end
    endcase
  end
 
@@ -54,7 +60,8 @@ module syn_fifo #(parameter DEP=4, parameter WID=16)
     if(!rst)
       fifo_mem<=0;
     else if(wr_q)
-      fifo_mem[wrptr]<=wdata;
+      fifo_mem[wrptr[PTR_WID-1:0]]<=wdata;
   end
 
 endmodule  
+  
